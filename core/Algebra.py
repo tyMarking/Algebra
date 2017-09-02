e79345673c786121f4ed3dfb6f8a96094447ce37
Ty Marking <tymarking@gmail.com>
2017-09-01 20:02:30 -0700

    The beginings of the project

---
+++ core/Algebra.py
@@ -0,0 +1,3 @@
+#Solves for Variable in expression
+def solveFor(variable, expression):
+    
---
+++ core/__init__.py
@@ -0,0 +1,11 @@
+
+
+
+def main():
+    print("hi")
+    print("sup")
+
+#This is a test
+
+if __name__ == "__main__":
+    main()-
